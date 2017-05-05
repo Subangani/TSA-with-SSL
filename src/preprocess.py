5b3fa3c7faf31224174b0a955db6f0cfe0921eed
Subangani <subangani1993@gmail.com>
2017-05-05 12:53:09 +0530

    postagging feature extraction

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,8 @@
 import re
 import csv
+import postag
+import ngramGenerator
+import nltk
 #start getStopWordList
 def loadStopWordList():
     fp =  open("../resource/stopWords.txt",'r')
@@ -23,6 +26,30 @@ def removeStopWords(tweet,stopWords):
         else:
             result=result+w+' '
     return result
+
+def negate(tweets):
+    negationList = ["never", "no", "nothing", "nowhere", "noone", "none", "not", "havent", "hasnt", "hadnt", "cant",
+                    "couldnt", "shouldnt", "wont", "wouldnt", "dont", "doesnt", "didnt", "isnt", "arent", "aint","haven't",
+                    "hasn't", "hadn't", "can't","couldn't", "shouldn't", "wouldn't", "don't", "doesn't", "didn't", "isn't",
+                    "aren't", "ain't"]
+    puncuationMarks = [".", ":", ";", "!", "?"]
+    for i in range(len(tweets)):
+        if tweets[i] in negationList:
+            #print "yes we found u killer ................... " + tweets[i]
+            j = i+1
+            #counter=0
+            while j < len(tweets):
+                if (tweets[j][-1] not in puncuationMarks):
+                    tweets[j] = tweets[j] + "_NEG"
+                    j = j +1
+                    #counter=1
+                else:
+                    break
+            i = j
+            #if (counter==1):
+             #   tweets[i] = tweets[i] + "_NEG"
+            #print tweets
+    return tweets
 
 
 
@@ -73,28 +100,51 @@ def preProcessTweet(tweet): # arg tweet, stopWords list and internet slangs dict
     processedTweet=replaceTwoOrMore(tweet) # replace multi-occurences by two
     slangs = loadInternetSlangsList()
     words=replaceSlangs(processedTweet,slangs).split()
+    negatedTweets=negate(words)
     stopWords = loadStopWordList()
-    preprocessedtweet = removeStopWords(words,stopWords)
-    return preprocessedtweet
+    preprocessedtweet = removeStopWords(negatedTweets,stopWords)
+   # print preprocessedtweet
+    postaggedTweet = postag.posTag(preprocessedtweet)
+    #print postaggedTweet
+    return postaggedTweet
+
 #end
 
-def process(filename, preprocessedFilename):
+def process(filename, positiveUnigram,positiveBigram,positiveTrigram):
     f0=open(filename,"r")
-    f1 = open(preprocessedFilename,"w")
+    f1 = open(positiveUnigram,"w")
+    f2 = open(positiveBigram, "w")
+    f3 = open(positiveTrigram, "w")
     reader = csv.reader(f0)
     for row in reader:
         a = row[2]
         tweet= preProcessTweet(a)
-        f1.write(tweet + '\n')
+        tweetUnigram = ngramGenerator.getSortedWordCount(tweet,1)
+        for i in range(len(tweetUnigram)):
+            f1.write(''.join('%s ' %  tweetUnigram[i][0]) +"\n")
+        tweetBigram = ngramGenerator.getSortedWordCount(tweet,2)
+        for i in range(len(tweetBigram)):
+            f2.write(''.join('%s ' % str(tweetBigram[i][0]))+ "\n")
+        tweetTrigram = ngramGenerator.getSortedWordCount(tweet,3)
+        for i in range(len(tweetTrigram)):
+            f3.write(''.join('%s ' %  str(tweetTrigram[i][0])) +"\n")
+
     f0.close()
     f1.close()
+    f2.close()
+    f3.close()
 
 #pre-processing positive twits
 positiveFilename= "../dataset/positive.csv"
-positivePreprocessedFilename = "../dataset/positiveProcessed.txt"
+positiveUnigramfile = "../dataset/positiveUnigram.txt"
+positiveBigramfile = "../dataset/positiveBigram.txt"
+positiveTrigramfile = "../dataset/positiveTrigram.txt"
 print "preprocessing positive tweets"
-process(positiveFilename,positivePreprocessedFilename)
+process(positiveFilename,positiveUnigramfile,positiveBigramfile,positiveTrigramfile)
 
+
+
+"""
 #pre-processing negative twits
 negativeFilename= "../dataset/negative.csv"
 negativePreprocessedFilename = "../dataset/negativeProcessed.txt"
@@ -106,6 +156,4 @@ neutralFilename= "../dataset/neutral.csv"
 neutralPreprocessedFilename = "../dataset/neutralProcessed.txt"
 print "preprocessing neutral tweets"
 process(neutralFilename,neutralPreprocessedFilename)
-
-
-
+"""
